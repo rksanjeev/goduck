03dddbedfe0d8561e8955caeaac94b9cc8743122
Sanjeev Kumar <sanjeevkumar@Mac.local>
2025-12-14 21:11:46 +0000

    s3 conn and go mod

--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import (
 	"context"
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/duckdb/duckdb-go/v2"
 )
 
 func main() {
 	var s3_prefix string = "s3://5b02238f-3466-44bc-8131-71576d473f97/"
+	var tmpStoragePath string = "/tmp/duck/storage"
+
+	// Create temporary storage directory
+	err := os.MkdirAll(tmpStoragePath, os.ModePerm)
+	if err != nil {
+		log.Fatal("Error creating temporary storage directory: ", err)
+	}
+
 	// Open database connection
 	db, err := sql.Open("duckdb", "")
 	if err != nil {
@@ -32,10 +41,14 @@ func main() {
         TYPE s3,
         PROVIDER credential_chain
     );
+    SET temp_directory='`+tmpStoragePath+`';
+    SET max_temp_directory_size = '10GB';
+    SET threads=4;
+    PRAGMA memory_limit='8GB';
 	`)
 
 	if err != nil {
-		log.Fatal("Error configuring S3: ", err)
+		log.Fatal("Error configuring storage and S3: ", err)
 	}
 
 	// Enable profiling
@@ -50,50 +63,33 @@ func main() {
 
 	// Corrected query with proper JOINs
 	query := `CREATE TABLE result_table AS
-SELECT
-    CAST(o.customer_id AS VARCHAR) AS customer_id,
-    LIST(
-        STRUCT_PACK(
-            productid := oi.product_id,
-            product_name := p.product_name,
-            quantity := oi.quantity,
-            unit_price := oi.unit_price
-        )
-    ) AS ordered
-FROM '` + s3_prefix + `data/parquet/orders.parquet' o
-JOIN '` + s3_prefix + `data/parquet/order_items.parquet' oi
-    ON o.order_id = oi.order_id
-JOIN '` + s3_prefix + `data/parquet/products.parquet' p
-    ON oi.product_id = p.product_id
-GROUP BY o.customer_id;
+			SELECT
+				CAST(o.customer_id AS VARCHAR) AS customer_id,
+				LIST(
+					STRUCT_PACK(
+						productid := oi.product_id,
+						product_name := p.product_name,
+						quantity := oi.quantity,
+						unit_price := oi.unit_price
+					)
+				) AS ordered
+			FROM '` + s3_prefix + `data/parquet/orders.parquet' o
+			JOIN '` + s3_prefix + `data/parquet/order_items.parquet' oi
+				ON o.order_id = oi.order_id
+			JOIN '` + s3_prefix + `data/parquet/products.parquet' p
+				ON oi.product_id = p.product_id
+			GROUP BY o.customer_id;
 
-COPY result_table
-TO '` + s3_prefix + `data/result/result.json'
-(ARRAY);
-
-
-
-`
+			COPY result_table
+			TO '` + s3_prefix + `data/result/result.json'
+			(ARRAY);
+			`
 
 	// Execute query and get result
 	_, err = conn.ExecContext(context.Background(), query)
 	if err != nil {
 		log.Fatal("Error executing query: ", err)
 	}
-	//	defer res.Close()
-
-	// Process results (for demonstration, let's just print out the rows)
-	//	for res.Next() {
-	//		var orderID, customerID, productID, quantity int
-	//		var unitPrice float64
-	//		var productName string
-	//		err = res.Scan(&orderID, &customerID, &productID, &unitPrice, &quantity, &productName)
-	//		if err != nil {
-	//			log.Fatal("Error scanning result: ", err)
-	//		}
-	//		fmt.Printf("Order ID: %d, Customer ID: %d, Product ID: %d, Unit Price: %f, Quantity: %d, Product Name: %s\n", orderID, customerID, productID, unitPrice, quantity, productName)
-	//	}
-
 	// Disable profiling
 	_, err = conn.ExecContext(context.Background(), `PRAGMA disable_profiling`)
 	if err != nil {
